8303cc97887467e41f5233214c2dac5f5539648f
Kallfaktorn <mattiaspgames@gmail.com>
2010-11-29 11:43:01 +0100

    Code cleanup.

--- game.erl
+++ game.erl
@@ -1,16 +1,24 @@
+%Header
+
 -module(game).
 -import(lists).
 -import(random).
--export([new_game/0,new_player/1,turn/2,test/0]).
+-export([example_new_game/0,new_player/1,turn/2,test/0]).
+
+
+%Test
 
 test() ->
-    new_game(),
+    example_new_game(),
 	new_player(p1),
 	new_player(p2),
 	new_player(p3),
 	turn(p1,pirch),
 	turn(p2,pike),
 	turn(p3,pirch).
+
+
+%Structure
 
 -record(game,
     {game_data,
@@ -32,6 +40,8 @@ test() ->
 -record(lake_state,
     {lake}).
 
+
+%Inititialize
 
 create_game(NrOfPlayers, NrOfTurns) ->
     GameData     = create_game_data(NrOfPlayers, NrOfTurns),
@@ -59,66 +69,98 @@ create_empty_players_state() ->
 create_lake_state() -> #lake_state{lake = pick_random(lakes())}.
 
 
-new_game() -> case whereis(game_pid) of
-                  undefined -> NrOfPlayers = 2,
-				               NrOfTurns   = 2, %Unused. For now...
-				               Game = create_game(NrOfPlayers,NrOfTurns), 
-				               GamePid = spawn( fun() ->
-				                                    process_flag(trap_exit, true),
-												    loop(Game)
-										            end ),
-				               register(game_pid, GamePid);
-                  GamePid  -> io:format("The game is already started with Pid: ~w ~n", [GamePid])
-			  end.
+create_new_player(Game, Name) ->
+    create_player(Game, Name, 0).
 
-player() ->
-	player(). 
+create_player(Game, Name, Value) ->
+    PlayerPid      = spawn_player(Name),
+    GameWithPlayer = add_player(Game, PlayerPid, Value),
+    GameWithPlayer.
+
+spawn_player(Name) ->
+    PlayerPid = spawn_link(fun() -> player() end ),
+    register(Name,PlayerPid),
+    PlayerPid.
+
+add_player(Game, PlayerPid, Value) ->
+    {players_state, BeforeTurn, AfterTurn}  = Game#game.players_state,
+	GameWithAddedPlayer = Game#game{players_state = {players_state, 
+                                                     [{PlayerPid, Value}|BeforeTurn],
+                                                     AfterTurn}},
+    GameWithAddedPlayer.
+
+
+%Startup
+
+new_game(NrOfPlayers, NrOfTurns) -> 
+    case whereis(game_pid) of
+        undefined ->
+            Game = create_game(NrOfPlayers,NrOfTurns), 
+            GamePid = spawn( fun() ->
+                process_flag(trap_exit, true),
+                loop(Game) end ),
+                register(game_pid, GamePid);
+        GamePid  -> io:format("The game is already started with Pid: ~w ~n", [GamePid])
+    end.
+
+
+example_new_game() ->
+    new_game(2,2).
 
 
 new_player(Name) ->
     game_pid ! {join, self(), Name},
-	receive
-	{'Full'} -> io:format("No more room.");
-	{'Ok', _} -> io:format("Welcome: ~w.~n" ++ 
-		                     "Enter game:turn(~w, Parameter). to make your turn.~n" ++
-							 "Parameter is either pirch or pike.~n~n"
-							 , [Name,Name])
+    receive
+        {'Full'} -> 
+            io:format("No more room.~n");
+        {'Ok'} -> 
+            io:format("Welcome: ~w.~n" ++ 
+                      "Enter game:turn(~w, Parameter). to make your turn.~n" ++
+                      "Parameter is either pirch or pike.~n~n"
+                     , [Name,Name])
     end.
 
 
+%Loop
+
 loop(Game) ->
     receive
-	{join,Pid,Name}    -> case space_left(Game) of
-	                          true  -> PlayerPid = spawn_link( fun() -> player() end ),
-						               register(Name,PlayerPid),
-						      	       Value = 0,
-							           ModifiedGame = add_player(Game, PlayerPid, Value),
-						               Pid ! {'Ok', PlayerPid},
-								       loop(ModifiedGame);
-						      false -> Pid ! {'Full'},
-							           loop(Game)
-							  end;
-    {play,Pid,Hunting} -> GameSt1 = play(Game,Pid,Hunting),      %Confirm Player move.
-                          case GameSt1 of
-    					  nix -> loop(Game);
-						  _   -> GameSt2 = tryEvaluate(GameSt1), %Evaluate if all players have moved.
-						         loop(GameSt2)
-    					  end;
-	{print, _}           -> printPlayers(Game),
-	                      loop(Game)
+        {join,Pid,Name} ->
+            case space_left(Game) of
+                true  -> 
+                    ModifiedGame = create_new_player(Game, Name),
+                    Pid ! {'Ok'},
+                    loop(ModifiedGame);
+                false -> 
+                    Pid ! {'Full'},
+                    loop(Game)
+            end;
+        {play,Pid,Hunting} -> 
+            GameSt1 = play(Game,Pid,Hunting),
+            case GameSt1 of
+                nix -> 
+                    loop(Game);
+                _   -> 
+                    GameSt2 = update(GameSt1), 
+                    loop(GameSt2)
+            end;
+        {print, _} -> 
+            printPlayers(Game),
+            loop(Game)
 	end.
 
 
+player() ->
+	player(). 
+    
+
+%Gameplay
+
 turn(Name, Hunting) ->
     case whereis(Name) of
-	    undefined -> io:format("Invalid name.");
+	    undefined -> io:format("Invalid name.~n");
 		Pid       -> game_pid ! {play, Pid, Hunting}
-		end.
-
-
-
-lakes() -> [[pirch,pike],[pirch,pirch],[pirch],[pike]].
-
+    end.
 
 
 space_left(Game) -> 
@@ -127,64 +169,57 @@ space_left(Game) ->
 	length(Performing) < Nop.
     
 
-add_player(Game, PlayerPid, Value) ->
-    {players_state, BeforeTurn, AfterTurn}  = Game#game.players_state,
-    %{{players_state, Performing, Waiting}, GameRest} = remove(players_state, Game),
-	GameWithAddedPlayer = Game#game{players_state = {players_state, [{PlayerPid, Value}|BeforeTurn], AfterTurn}},
-    GameWithAddedPlayer.
-	%[{players_state, [{PlayerPid, Value}|Performing], Waiting}|GameRest].
-
-
+%Game logic
 
 play(Game, Pid, Hunting) ->
-    io:format("Test"),
-    %{{players_state, Performing, Waiting}, GameRest} = remove(players_state, Game),
 	{players_state, Performing, Waiting} = Game#game.players_state,
 	case remove(Pid, Performing) of
-	{{Pid, Value}, PerformingRest}  ->
-	    GameChanged = Game#game{players_state = {players_state, PerformingRest, [{Pid, Hunting, Value}|Waiting]}},
-		GameChanged;
-	    %[{players_state, PerformingRest, [{Pid, Hunting, Value}|Waiting]}|GameRest];
-	nix -> io:format("Error: Not yet players turn or player does not exist.~n"),
-	       nix
+	    {{Pid, Value}, PerformingRest}  ->
+            GameChanged = Game#game{players_state = {players_state, 
+                                                     PerformingRest, 
+                                                     [{Pid, Hunting, Value}|Waiting]}},
+            GameChanged;
+        nix -> 
+            io:format("Error: Not yet players turn or player does not exist.~n"),
+	        nix
 	end.
 
-tryEvaluate(Game) ->
-    %{{players_state, Performing, _}, _} = remove(players_state, Game),
+update(Game) ->
 	{players_state, Performing, _} = Game#game.players_state,
 	case Performing of 
-	    [] -> GameRest = evaluate(Game),
-		      printPlayers(GameRest),
-			  GameRest;
+	    [] -> GameNewTurn = evaluate(Game),
+		      printPlayers(GameNewTurn),
+			  GameNewTurn;
         _  -> Game
 	end.
 
 evaluate(Game) ->
-  %{{lake_state, Fishes}, GameRest1}         = remove(lake_state, Game),
-  {lake_state, Fishes} = Game#game.lake_state,
-  %{{players_state, _, Players},GameRest2} = remove(players_state, GameRest1),
-  {players_state, _, Players} = Game#game.players_state,
-  %evaluate(GameRest2, Players, [], Fishes).
-  evaluate(Game, Players, [], Fishes).
+    {lake_state, Fishes} = Game#game.lake_state,
+    {players_state, _, Players} = Game#game.players_state,
+    evaluate(Game, Players, [], Fishes).
 
-
-
-    
 evaluate(Game, [], EvalPlayers, _) ->
   GameLakeUpdate        = Game#game{lake_state = create_lake_state()},
   GameLakePlayersUpdate = GameLakeUpdate#game{players_state = {players_state, EvalPlayers, []}},
   GameLakePlayersUpdate;
-  %[create_lake_state()|[{players_state, EvalPlayers, []}|Game]];
+
 evaluate(Game, Players, EvalPlayers, Fishes) ->
-  {Pid, Hunting, Value} = pick_random(Players),
-  {_, PlayersRest} = remove(Pid,Players),
-  case removeFromList(Hunting, Fishes) of
-  {Fish, FishesRest} -> evaluate(Game,PlayersRest,[{Pid,valueOf(Fish)+Value}|EvalPlayers],FishesRest);
-  _                  -> evaluate(Game,PlayersRest,[{Pid,Value}|EvalPlayers],Fishes)
-  end.
+    {Pid, Hunting, Value} = pick_random(Players),
+    {_, PlayersRest}      = remove(Pid,Players),
+    case removeFromList(Hunting, Fishes) of
+        {Fish, FishesRest} -> 
+            evaluate(Game,
+            PlayersRest,
+            [{Pid,valueOf(Fish)+Value}|EvalPlayers],
+            FishesRest);
+        _ -> 
+            evaluate(Game,PlayersRest,[{Pid,Value}|EvalPlayers],Fishes)
+    end.
+
+
+%Output
 
 printPlayers(Game) ->
-    %{{players_state, Performing,_}, _} = remove(players_state, Game),
 	{players_state, Performing, _} = Game#game.players_state,
 	printPlayersStat(Performing).
 
@@ -193,21 +228,20 @@ printPlayersStat([{Pid,Value}|Players]) ->
     io:format("Player: ~w Points: ~w. ~n",[Pid,Value]),
 	printPlayersStat(Players).
 
+
+%Game properties
 valueOf(Fish) ->
- case Fish of
- pirch -> 1;
- pike  -> 2
- end.
+    case Fish of
+        pirch -> 1;
+        pike  -> 2
+    end.
 
-%test() ->
-%  new_game(),
-%  new_game(),
-%  new_player('mat'),
-%  new_player(mat2),
-%  new_player(mat3).
 
+lakes() -> [[pirch,pike],[pirch,pirch],[pirch],[pike]].
 
 %Personal library
+
+
 %Given an element e and a list l.
 %If e is in list, return a tuple consisting of the element e and
 %the list without element e: {e,restOfList}
@@ -230,8 +264,8 @@ remove(_,[],_) -> nix;
 remove(Atom,[Tpl|List], Tmp) ->
     TplAtom = element(1,Tpl),
 	case TplAtom == Atom of
-	true  -> {Tpl, Tmp ++ List};
-	false -> remove(Atom,List,[Tpl|Tmp])
+        true  -> {Tpl, Tmp ++ List};
+        false -> remove(Atom,List,[Tpl|Tmp])
 	end.
 
 %Given a list, return a random element from the list.
